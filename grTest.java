b5a7c46bc5f0ffce1ac081c49f31c7a547c64b3f
payas <relekarpayas@gmail.com>
2017-09-08 08:41:48 +0530

    Make three moving circles.

--- grTest.java
+++ grTest.java
@@ -4,9 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class grTest extends JFrame implements Runnable {
-    private int x = 10, y = 100;
+    private int x1=10, y1=100, x2=10, y2=200, x3=10, y3=300;
 
     private grTest() throws HeadlessException {
+        super();
+
         this.setSize(700, 500);
         this.setVisible(true);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -15,23 +17,39 @@ public class grTest extends JFrame implements Runnable {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
+
         g.setColor(Color.BLUE);
-        g.fillOval(this.x, this.y, 50, 50);
+        g.fillOval(this.x1, this.y1, 50, 50);
+
+        g.setColor(Color.RED);
+        g.fillOval(this.x2, this.y2, 50, 50);
+
+        g.setColor(Color.GREEN);
+        g.fillOval(this.x3, this.y3, 50, 50);
     }
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(new grTest());
+        Runnable r1 = new grTest();
+        Thread t1 = new Thread(r1,"BLUE");
+        Thread t2 = new Thread(r1,"RED");
+        Thread t3 = new Thread(r1,"GREEN");
 
         t1.start();
+        t2.start();
+        t3.start();
     }
 
     @Override
     public void run() {
-        for (; this.x < 500; this.x++) {
-            this.x++;
+
+        for (;this.x1<500 |this.x2<500|this.x3<500; ) {
+
+            this.x1++;
+            this.x2++;
+            this.x3++;
 
             try {
-                Thread.sleep(25);
+                Thread.sleep(50);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
